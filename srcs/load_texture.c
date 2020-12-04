353e7bd53b177bd2cbcf7f7d1c90738c9d1a99cc
Alvaro Gutierrez martinez <agutierr@c2r15s6.42madrid.com>
2020-12-04 21:38:44 +0100

    sin fallos, solo abre ventana y esta casi el rc

--- srcs/load_texture.c
+++ srcs/load_texture.c
@@ -21,33 +21,33 @@ void load_textures(t_mlx *mlx)
 			&mlx->rc.tex_width, &mlx->rc.tex_height);
 	mlx->rc.tex[4].addr = mlx_get_data_addr(mlx->rc.tex[4].img, &mlx->rc.tex[4].bpp,
 			&mlx->rc.tex[4].linesize, &mlx->rc.tex[4].endian);
-	mlx->rc.tex[5].img = mlx_xpm_file_to_image(mlx->ptr, "sprites/pillar.xpm",
+	mlx->rc.tex[5].img = mlx_xpm_file_to_image(mlx->ptr, "textures/sprites/pillar.xpm",
 			&mlx->rc.tex_width, &mlx->rc.tex_height);
-//	mlx->rc.tex[5].addr = mlx_get_data_addr(mlx->rc.tex[5].img, &mlx->rc.tex[5].bpp,
-//			&mlx->rc.tex[5].linesize, &mlx->rc.tex[5].endian);
+	mlx->rc.tex[5].addr = mlx_get_data_addr(mlx->rc.tex[5].img, &mlx->rc.tex[5].bpp,
+			&mlx->rc.tex[5].linesize, &mlx->rc.tex[5].endian);
 	load_textures_2(mlx);
 }
 
 static void load_textures_2(t_mlx *mlx)
 {
-	mlx->rc.tex[6].img = mlx_xpm_file_to_image(mlx->ptr, "sprites/barrel.xpm",
+	mlx->rc.tex[6].img = mlx_xpm_file_to_image(mlx->ptr, "textures/sprites/barrel.xpm",
 			&mlx->rc.tex_width, &mlx->rc.tex_height);
-//	mlx->rc.tex[6].addr = mlx_get_data_addr(mlx->rc.tex[6].img, &mlx->rc.tex[6].bpp,
-//			&mlx->rc.tex[6].linesize, &mlx->rc.tex[6].endian);
-	mlx->rc.tex[7].img = mlx_xpm_file_to_image(mlx->ptr, "sprites/light.xpm",
+	mlx->rc.tex[6].addr = mlx_get_data_addr(mlx->rc.tex[6].img, &mlx->rc.tex[6].bpp,
+			&mlx->rc.tex[6].linesize, &mlx->rc.tex[6].endian);
+	mlx->rc.tex[7].img = mlx_xpm_file_to_image(mlx->ptr, "textures/sprites/light.xpm",
 			&mlx->rc.tex_width, &mlx->rc.tex_height);
-//	mlx->rc.tex[7].addr = mlx_get_data_addr(mlx->rc.tex[7].img, &mlx->rc.tex[7].bpp,
-//			&mlx->rc.tex[7].linesize, &mlx->rc.tex[7].endian);
-	mlx->rc.tex[8].img = mlx_xpm_file_to_image(mlx->ptr, "sprites/ghostred.xpm",
+	mlx->rc.tex[7].addr = mlx_get_data_addr(mlx->rc.tex[7].img, &mlx->rc.tex[7].bpp,
+			&mlx->rc.tex[7].linesize, &mlx->rc.tex[7].endian);
+	mlx->rc.tex[8].img = mlx_xpm_file_to_image(mlx->ptr, "textures/sprites/ghostred.xpm",
 			&mlx->rc.tex_width, &mlx->rc.tex_height);
-//	mlx->rc.tex[8].addr = mlx_get_data_addr(mlx->rc.tex[8].img, &mlx->rc.tex[8].bpp,
-//			&mlx->rc.tex[8].linesize, &mlx->rc.tex[8].endian);
-	mlx->rc.tex[9].img = mlx_xpm_file_to_image(mlx->ptr, "sprites/destruction.xpm",
+	mlx->rc.tex[8].addr = mlx_get_data_addr(mlx->rc.tex[8].img, &mlx->rc.tex[8].bpp,
+			&mlx->rc.tex[8].linesize, &mlx->rc.tex[8].endian);
+	mlx->rc.tex[9].img = mlx_xpm_file_to_image(mlx->ptr, "textures/sprites/destruction.xpm",
 			&mlx->rc.tex_width, &mlx->rc.tex_height);
-//	mlx->rc.tex[9].addr = mlx_get_data_addr(mlx->rc.tex[9].img, &mlx->rc.tex[9].bpp,
-//			&mlx->rc.tex[9].linesize, &mlx->rc.tex[9].endian);
-	mlx->rc.tex[10].img = mlx_xpm_file_to_image(mlx->ptr, "sprites/dead.xpm",
+	mlx->rc.tex[9].addr = mlx_get_data_addr(mlx->rc.tex[9].img, &mlx->rc.tex[9].bpp,
+			&mlx->rc.tex[9].linesize, &mlx->rc.tex[9].endian);
+	mlx->rc.tex[10].img = mlx_xpm_file_to_image(mlx->ptr, "textures/sprites/dead.xpm",
 			&mlx->rc.tex_width, &mlx->rc.tex_height);
-//	mlx->rc.tex[10].addr = mlx_get_data_addr(mlx->rc.tex[10].img, &mlx->rc.tex[10].bpp,
-//			&mlx->rc.tex[10].linesize, &mlx->rc.tex[10].endian);
+	mlx->rc.tex[10].addr = mlx_get_data_addr(mlx->rc.tex[10].img, &mlx->rc.tex[10].bpp,
+			&mlx->rc.tex[10].linesize, &mlx->rc.tex[10].endian);
 }
